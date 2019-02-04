5358c760882006d22b7e45556ed45d8736049fd5
Jason Koirala <jasonkoirala@Jasons-MacBook-Pro.local>
2019-02-04 13:42:54 -0600

    Final

--- src/dao/BillDAO.java
+++ src/dao/BillDAO.java
@@ -1,11 +1,14 @@
 package dao;
 
+import java.util.List;
+
 import ORM.DbContext;
 import SqlQueryBuilder.DynamicQueryBuilder;
 import SqlQueryBuilder.QueryDirector;
 import entities.Charge;
 import entities.Entity;
 import entities.Patient;
+import entities.User;
 
 public class BillDAO extends DbContext {
 	private Charge charge;
@@ -16,6 +19,14 @@ public class BillDAO extends DbContext {
 		QueryDirector qd = new QueryDirector(new DynamicQueryBuilder(charge));
 		qd.constructQuery();
 		super.setQuery(qd.getQuery());
+	}
+	
+	public List<Charge> getBillDetailsByName(String name) {
+		String query = "SELECT FROM CHARGE WHERE patientId= '"+name+"'";
+		super.setSql(query);
+		List<Charge> charges = (List<Charge>) super.CustomReadAction();
+		return charges;
+		
 	}
 
 	@Override
